0bad2b8cdd7692d75cbcd814ed2a7756e165932d
Júlio Assis <juliosevarollia@gmail.com>
2019-02-26 20:19:36 +0100

    solution ex2 A e B

--- q2_svm_python/svmlin.py
+++ q2_svm_python/svmlin.py
@@ -23,4 +23,54 @@ def svmlin(X, t, C):
 
 
     #####Insert your code here for subtask 2a#####
+
+    # Get dimensions of data
+    num_samples, dim = X.shape
+
+    # Construct quadratic matrix of the problem
+    H = np.zeros((num_samples, num_samples))
+    for n in range(num_samples):
+      H[n,n] = t[n] * t[n] * np.dot(X[n, :], X[n, :])
+      for m in range(n+1, num_samples):
+        H[n,m] = t[n] * t[m] * np.dot(X[n, :], X[m, :])
+        H[m,n] = H[n,m]
+    
+    # vector to sum the lagrange multipliers in the objective function
+    q = (-1) * np.ones(num_samples)
+
+    # equation for the upper and lower boundaries
+    G = np.vstack([-np.eye(num_samples,dtype=np.float),
+     np.eye(num_samples, dtype=np.float)])
+    h = np.hstack([np.zeros((num_samples)),
+     C*np.ones((num_samples))]).reshape(-1,1).astype(np.float)
+
+    # complementary slackness
+    A = t.reshape(1,-1).astype(np.float)
+    b = 0.0
+
+    # get lagrange multipliers
+    alpha = cvxopt.solvers.qp(P=cvxopt.matrix(H), 
+                              q=cvxopt.matrix(q), 
+                              G=cvxopt.matrix(G), 
+                              h=cvxopt.matrix(h), 
+                              A=cvxopt.matrix(A), 
+                              b=cvxopt.matrix(b))
+    alpha = np.array(alpha["x"])
+    
+    # support vectors and weights
+    sv = (alpha > 1e-6).reshape(-1)
+    w = np.zeros((dim,))
+    for n in range(num_samples):
+      w += alpha[n] * t[n] * X[n, :]
+
+    # determining the bias term
+    sv_idxs = np.array(range(len(sv)))
+    for idx in sv_idxs[sv]:
+        b += t[idx] - np.dot(w, X[idx, :])
+    b /= t[sv].shape[0]
+
+    # support vectors on the border
+    slack = alpha == C
+    result = np.dot(X, w) + b
+
     return alpha, sv, w, b, result, slack
